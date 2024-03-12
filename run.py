86423bf4aea4f79a78d895cc46f3f980640cfbc8
CallumNewman <ballumnew@gmail.com>
2024-03-12 23:54:28 +0000

    retrieved latest stock numbers

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import pprint
 import gspread 
 from google.oauth2.service_account import Credentials
 
@@ -59,6 +60,26 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully!")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    the surplus is defined as the sales figure subtracted from the stock:
+    - positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+main()
